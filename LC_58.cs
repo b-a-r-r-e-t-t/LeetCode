7eea3d49a18c57de5a2250a8a0b912f867d00b06
Will Barrett <wbarrett13+github@live.com>
2024-05-14 01:36:42 -0400

    LC 58

---
+++ LC_58.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCode;
+
+public class LC_58
+{
+    public int LengthOfLastWord_Old(string s){
+        int length = 0;
+        char[] chars = s.Trim().ToCharArray();
+        while(length < chars.Length) {
+            if(!Char.IsWhiteSpace(chars[chars.Length - 1 - length])){ 
+                length++;
+            } else {
+                break;
+            }
+        }
+        return length;
+    }
+
+    public int LengthOfLastWord(string s){
+        int length = 0;
+        int skip = 0;
+        while(length < s.Length) {
+            if(length == skip && Char.IsWhiteSpace(s[s.Length - 1 - length])){ 
+                skip++; length++;
+                continue;
+            } else if(!Char.IsWhiteSpace(s[s.Length - 1 - length])){ 
+                length++;
+            } else {
+                break;
+            }
+        }
+        return length - skip;
+    }
+    public int LengthOfLastWord_Regex(string s){
+        Match match = Regex.Match(s, @"\b\w+\b(?=\W*$)");
+        return match.Length;
+        //\b\w+\b(?=\W*$)
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        LC_121 test = new();
-        LC_13 lC_13 = new();
-        Console.WriteLine(lC_13.RomanToInt("LVIII"));
+        //LC_58 lC_58 = new();
+        //Console.WriteLine(lC_58.LengthOfLastWord("HelloWorld  "));
+
+        //LC_121 test = new();
         //Console.WriteLine(test.SellStocks([7,1,5,3,6,4]));
+
+        //LC_13 lC_13 = new();
+        //Console.WriteLine(lC_13.RomanToInt("LVIII"));
     }
 }
